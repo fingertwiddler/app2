2a5087bbf9a9bc1a908cbf4f772210700b225310
fingertwiddler <fingertwiddler@gmail.com>
2021-01-30 18:34:05 -0500

    x

--- engine/builder.js
+++ engine/builder.js
@@ -5,6 +5,8 @@ export class Builder {
     this.config = o.config
 //    this.modules = o.modules
     this.src = o.src
+    this.fs = o.fs
+    this.git = o.git
   }
   /***********************************************************************
   *
@@ -22,14 +24,14 @@ export class Builder {
       let url = m[2]
       let match = /assets\/(.+)$/.exec(url)
       if (match && match.length > 0) {
-        let b = await fs.promises.readFile(`${config.SRC}/assets/${match[1]}`)
-        await fs.promises.writeFile(`${config.DEST}/assets/${match[1]}`, b)
+        let b = await this.fs.promises.readFile(`${config.SRC}/assets/${match[1]}`)
+        await this.fs.promises.writeFile(`${config.DEST}/assets/${match[1]}`, b)
       }
     }
   }
   async processContent(o, config) {
     // Build full HTML page for the post and write to DEST
-    let theme = await fs.promises.readFile(config.THEME.POST, "utf8")
+    let theme = await this.fs.promises.readFile(config.THEME.POST, "utf8")
     let template = Handlebars.compile(theme)
     let rendered = template({
       name: config.NAME,
@@ -38,12 +40,12 @@ export class Builder {
       filename: `${config.SRC}/${o.filename}`,
       title: o.data.title
     }).trim()
-    await fs.promises.mkdir(`${config.DEST}/${o.filename}`).catch((e) => { })
-    await fs.promises.writeFile(`${config.DEST}/${o.filename}/index.html`, rendered)
+    await this.fs.promises.mkdir(`${config.DEST}/${o.filename}`).catch((e) => { })
+    await this.fs.promises.writeFile(`${config.DEST}/${o.filename}/index.html`, rendered)
   }
   async buildPost (filename, config) {
     // parse markdown metadata
-    let md = await fs.promises.readFile(`${config.SRC}/${filename}`, "utf8")
+    let md = await this.fs.promises.readFile(`${config.SRC}/${filename}`, "utf8")
     let { content, data } = matter(md)
     data.permalink = filename
     let html = marked(content, { baseUrl: "../" }) 
@@ -57,11 +59,11 @@ export class Builder {
     return { html, data }
   }
   async getPost (key) {
-    let md = await fs.promises.readFile(`${this.config.SRC}/${key}`, "utf8")
+    let md = await this.fs.promises.readFile(`${this.config.SRC}/${key}`, "utf8")
     return matter(md)
   }
   async build () {
-    let src = await fs.promises.readdir(this.config.SRC)
+    let src = await this.fs.promises.readdir(this.config.SRC)
     let items = await Promise.all(src.filter((key) => { return key !== "assets" }).map((key) => {
       return new Promise(async (resolve, reject) => {
         let { content, data } = await this.getPost(key) 
@@ -75,7 +77,7 @@ export class Builder {
     let publicItems = []
     for(let item of items) {
       if (item.data.draft) {
-        await fs.promises.unlink(`${this.config.DEST}/${key}/index.html`)
+        await this.fs.promises.unlink(`${this.config.DEST}/${key}/index.html`)
       } else {
         publicItems.push(item)
       }
@@ -89,7 +91,7 @@ export class Builder {
     }))
     for(let mod of modules) {
       await mod.default(publicItems, this.config, {
-        fs, git
+        this.fs, this.git
       })
     }
   }
